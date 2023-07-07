2ed5307b1af5e7dda431bfdb9fae5d0113c275af
adrummer1 <andrew@bluestemcreativeco.com>
2023-07-07 12:23:11 -0500

    Add scores to local storage

--- assets/gameend.js
+++ assets/gameend.js
@@ -3,13 +3,24 @@ var saveScoreBtn = document.getElementById("savedScore");
 var finalScore = document.getElementById("finalScore");
 var latestScore = localStorage.getItem("latestScore");
 
+var scores = JSON.parse(localStorage.getItem("scores"));
+
 finalScore.innerText = latestScore;
 
 initials.addEventListener("keyup", () => {
-    console.log(initials.value);
-    saveScoreBtn = !initials.value;
+    saveScoreBtn.disabled = !initials.value;
 });
 
 saveScore = (event) => {
     event.preventDefault();
+
+    var score = {
+        score: latestScore,
+        initials: initials.value
+    };
+    scores.push(score);
+    scores.sort( (a,b) => b.score - a.score);
+
+    localStorage.setItem("scores", JSON.stringify(scores));
+    // location.assign("/");
 };
