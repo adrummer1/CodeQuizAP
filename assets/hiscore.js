e0cc76edeafb16696e42cacd6f47f230f90ec99c
adrummer1 <andrew@bluestemcreativeco.com>
2023-07-12 13:37:13 -0500

    Detailed commenting on main js and hiscore js.

--- assets/hiscore.js
+++ assets/hiscore.js
@@ -1,8 +1,14 @@
+// get hiScoreList by element id
 var hiScoresList = document.getElementById("hiScoresList");
-var hiScores = JSON.parse(localStorage.getItem("scores")) || [];
 
-// Display list of high scores
+// parse score data from string to object
+var hiScores = JSON.parse(localStorage.getItem("scores"));
+
+// Display list of high scores and display initials with the score using the map function.
 hiScoresList.innerHTML = hiScores.map(score => {
+    // jQuery that sets the list data
     return `<li class="hi-score">${score.initials}: ${score.score}</li>`;
 })
+
+// Print initials and score so that each list item is on one line
 .join("");
--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,18 @@ var question = document.getElementById("question");
 var choice = Array.from(document.getElementsByClassName("choice-ans"));
 var timerEl = document.getElementById("timerEl");
 var scoreCounter = document.getElementById("scoreCounter");
+var initials = document.getElementById("initials");
+var saveScoreBtn = document.getElementById("savedScore");
+var finalScore = document.getElementById("finalScore");
+var currentQuestion = {};
+var acceptAnswers = false;
+var score = 0;
+var questionCount = 0;
+let availQuestions = [];
+var timer;
+var timerCount;
 
-// Function to set which pages display for the user
+// Function to set which div section displays for the user using jQuery
 function showPage(className) {
     $(".page").hide();
     $(className).show();
@@ -16,16 +26,8 @@ $(".btn-start").click(function () {
     showPage(".question");
 })
 
-let currentQuestion = {};
-let acceptAnswers = false;
-let score = 0;
-let questionCount = 0;
-let availQuestions = [];
-var timer;
-var timerCount;
-
-
-let questions = [
+// Setting data array for the four question in the quiz and an answer value that is a number.
+var questions = [
     {
         question: "Which of the following is not a primitive data type in JavaScript?",
         choice1: "Number",
@@ -60,11 +62,12 @@ let questions = [
     },
 ]
 
+// Set the point value to one and the maximum questions for the Next Question function
 var point = 1;
 var maxQuestions = 4;
 
 // Start game also starts timer and loads new question function
-startGame = () => {
+ function startGame () {
     timerCount = 100;
     score = 0;
     availQuestions = [...questions];
@@ -73,61 +76,101 @@ startGame = () => {
     getNextQuestion();
 };
 
-// Gets new question after answer submitted
-getNextQuestion = () => {
+// Gets new question after answer submitted. If the user gets through all four questions before the time ends take them to the quizend section.
+function getNextQuestion () {
     if (availQuestions.length === 0 || questionCount >= maxQuestions) {
         localStorage.setItem("latestScore", score);
-        return location.assign("./gameend.html");
+        return showPage(".quizend");
     }
+    // Iterates through the questions displays questions at random
     questionCount++;
     var questionIndex = Math.floor(Math.random() * availQuestions.length);
     currentQuestion = availQuestions[questionIndex];
     question.innerText = currentQuestion.question;
     choice.forEach(choice => {
+        // Set new variable for number that will relect the number assigned to the correct answer.
         var number = choice.dataset["number"];
         choice.innerText = currentQuestion["choice" + number];
     });
 
+    // Replaces one question with another available question from the question indexes.
     availQuestions.splice(questionIndex, 1);
 
+    // Change acceptAnswer variable to true when a user selects a choice.
     acceptAnswers = true;
 };
 
 // Adds correct or incorrect response depending on the user's answer
+// Sets new variable for response by getting the response element
 var response = document.getElementById("response");
+// This is an event function for when the user selects an answer it displays text below the question if the answer is correct on incorrect.
 choice.forEach(choice => {
     choice.addEventListener("click", Event => {
+        // If no choices are made this ends the function and returns a boolean of false.
         if (!acceptAnswers) return;
         acceptAnswers = false;
+        // Target the responses based on the number corresponding to the answer for each question.
         var pickedChoice = Event.target;
         var pickedAns = pickedChoice.dataset["number"];
+            // If the user chooses the correct answer return text Correct.
             if (pickedAns == currentQuestion.answer) {
                 var node1 = document.createTextNode("Correct!");
                 response.appendChild(node1);
                 score++;
                 scoreCounter.innerText = score;
+            // If the user chooses any other answer return text Incorrect and reduce the timer count by 20 seconds.
             } else {
                 var node2 = document.createTextNode("Incorrect");
                 response.appendChild(node2);
                 timerCount -= 20;
             }
 
-            setTimeout(() => {
+            // Set a brief one second pause that will remove the response text and take the user to the next question.
+            setTimeout( () => {
                 response.innerHTML = "";
                 getNextQuestion();
             }, 1000);
     });
 })
 
-// Start timer and take user to the game end page if timer run to zero
+// Start timer and take user to the gameend section if timer run to zero
 function startTimer() {
     timer = setInterval(function() {
         timerCount--;
         timerEl.textContent = timerCount;
         if (timerCount === 0) {
             clearInterval(timer);
-            location.replace("./gameend.html");
+            showPage(".quizend")
         }
     },1000);
 }    
+
+// Parse the score data to a js object and retrieve it from localStorage 
+var scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+// Print the latest score to the finalScore element
+var latestScore = localStorage.getItem("latestScore");
+finalScore.innerText = latestScore;
+
+//
+initials.addEventListener("keyup", () => {
+    saveScoreBtn.disabled = !initials.value;
+});
+
+// Save user's score and initials to localStorage when submitted and take user to the hiscore page.
+function saveScore (event) {
+    event.preventDefault();
+
+    var score = {
+        score: latestScore,
+        initials: initials.value
+    };
+    scores.push(score);
+    scores.sort( (a,b) => b.score - a.score);
+
+    localStorage.setItem("scores", JSON.stringify(scores));
+    location.assign("./hiscore.html");
+};
+
+// Call the startGame function.
 startGame();
