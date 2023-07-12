7568222959649c1b36ebe346818a5f2299ee2345
adrummer1 <andrew@bluestemcreativeco.com>
2023-07-12 17:51:26 -0500

    Added comments to hiscore js

--- assets/hiscore.js
+++ assets/hiscore.js
@@ -3,12 +3,15 @@ var hiScoresList = document.getElementById("hiScoresList");
 
 // parse score data from string to object
 var hiScores = JSON.parse(localStorage.getItem("scores"));
+console.log(hiScores)
 
-// Display list of high scores and display initials with the score using the map function.
+// Display list of scores.
+// Using the map method to iterate through localStorage array.
 hiScoresList.innerHTML = hiScores.map(score => {
-    // jQuery that sets the list data
+// jQuery that sets a new unordered list with localStorage data.
     return `<li class="hi-score">${score.initials}: ${score.score}</li>`;
 })
 
-// Print initials and score so that each list item is on one line
+// // Print initials and score so that each list item is on one line.
 .join("");
+
